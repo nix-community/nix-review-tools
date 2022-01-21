943bb009897c5bd461193e17f742f03b7bbb4c81
Samuel Dionne-Riel <samuel@dionne-riel.com>
2022-01-20 19:52:26 -0500

    Review packaging

---
+++ pkgs.nix
@@ -0,0 +1,4 @@
+import (builtins.fetchTarball {
+  url = "https://github.com/NixOS/nixpkgs/archive/6d8215281b2f87a5af9ed7425a26ac575da0438f.tar.gz";
+  sha256 = "00wy4zapg955yzpfnyx9lxm7jskmwyjzmdwr7d0mrwx3gl1wz9mx";
+}) { }
